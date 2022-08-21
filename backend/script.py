9c85846944fb14887ba27799ef94dd90f2a3f1b7
shy-tan <brownmetalhead.dev@gmail.com>
2022-08-21 22:26:58 +0500

    add scraper

--- backend/script.py
+++ backend/script.py
@@ -1,36 +1,81 @@
+import datetime
+import json
+from urllib import response
 import requests
 from bs4 import BeautifulSoup
 import html
+import os
+
 
 HEADERS = {
     "User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"
     }
 
+
 RSS_FEED = "https://tribune.com.pk/feed/latest"
 
 def fetch_rss(url):
+    all_news = []
+    output_json = {}
+    # {
+    #     date: [
+    #         {},
+    #         {}, 
+    #         {},
+    #     ]
+    # }
+
     try:
         r = requests.get(url, headers=HEADERS)
         soup = BeautifulSoup(r.content, features='xml')
         
         news = soup.find_all("item")
-        # print(soup)
-        for i, n in enumerate(news):
+        
+        for n in news:
             title = n.find('title').text
             link = n.find('link').text
             date = n.find('pubDate').text
             content = n.find('content:encoded').text
             image = n.find('image')
-           
-            # print(title)
-            # print(link)
-            # print(date)
-            # print(html.unescape(content))
-            print(list(image.children)[1]["src"])
-            # print(f"Item Number: {i+1}")
-            print("-"*50)
+            
+            article = {
+                'headline' : str(title),
+                'link' : str(link),
+                'date_published' : str(date),
+                'details' : str(html.unescape(content)),
+                'image_url' : str(list(image.children)[1]["src"])
+            }
 
-    except Exception as e:
-        print(e)
+            all_news.append(article) 
 
-fetch_rss(RSS_FEED)
+        current_timestamp = str(datetime.datetime.now())
+
+        output_json[current_timestamp] = all_news
+
+        return output_json
+    except Exception as e: 
+        return e
+
+
+
+def save_to_test_locally(output):
+
+    if 'output' not in os.listdir():
+        os.mkdir('output')
+    
+    os.chdir('output')
+
+    serialized_json = json.dumps(output, indent=4)
+    
+    with open('news.json', 'a') as out_json:
+        out_json.write(serialized_json)
+
+
+if __name__ == "__main__":
+
+    response = fetch_rss(RSS_FEED)
+
+    if isinstance(response, dict):
+        save_to_test_locally(response)
+    else:
+        print(response)
