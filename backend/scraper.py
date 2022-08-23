2db7ae6e81e624cf3ab36aeb93abad87614bb8f8
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-08-23 16:06:27 +0500

    fix bug in categories in  news-json-output

--- backend/scraper.py
+++ backend/scraper.py
@@ -7,50 +7,52 @@ import os
 
 
 HEADERS = {
-    "User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"
-    }
+    "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"
+}
 
 
 RSS_FEED = "https://tribune.com.pk/feed/"
 
+
 def fetch_rss(url):
-    all_news = []
     output_json = {}
-    categories = ['latest', 'world', 'analysis', 'politics', 'health', 'technology']
-    
+    categories = ['latest', 'world', 'analysis',
+                  'politics', 'health', 'technology']
+
     for category in categories:
+        all_news = []
         categ_url = url + category
         try:
             r = requests.get(categ_url, headers=HEADERS)
 
             soup = BeautifulSoup(r.content, features='xml')
-            
+
             news = soup.find_all("item")
-            
+
             for n in news:
                 title = n.find('title').text
                 link = n.find('link').text
                 date = n.find('pubDate').text
                 content = n.find('content:encoded').text
                 image = n.find('image')
-                
+
                 article = {
-                    'headline' : str(title),
-                    'link' : str(link),
-                    'date_published' : str(date),
-                    'details' : str(html.unescape(content)),
-                    'image_url' : str(list(image.children)[1]["src"])
+                    'headline': str(title),
+                    'link': str(link),
+                    'date_published': str(date),
+                    'details': str(html.unescape(content)),
+                    'image_url': str(list(image.children)[1]["src"])
                 }
 
-                all_news.append(article) 
+                all_news.append(article)
 
             output_json[category] = all_news
 
-
-        except Exception as e: 
+        except Exception as e:
             print(e)
 
     return output_json
+
 
 def save_to_test_locally(output):
 
@@ -60,6 +62,7 @@ def save_to_test_locally(output):
     with open('./output/news.json', 'w', encoding='utf-8') as out_json:
         json.dump(output, out_json, ensure_ascii=False)
 
+
 if __name__ == "__main__":
 
     response = fetch_rss(RSS_FEED)
@@ -68,6 +71,3 @@ if __name__ == "__main__":
         save_to_test_locally(response)
     else:
         print(response)
-
-
-    
